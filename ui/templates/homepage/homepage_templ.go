2bcdcf3f80a2dd3a6b97c95758616e58eaf2da54
GalacticPast <192919385+GalacticPast@users.noreply.github.com>
2026-05-12 16:42:48 -0400

    spacebar

--- ui/templates/homepage/homepage_templ.go
+++ ui/templates/homepage/homepage_templ.go
@@ -34,7 +34,7 @@ func Setup(container types.Color_container) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><link rel=\"stylesheet\" href=\"/static/styles.css\"><script type=\"module\" src=\"https://cdn.jsdelivr.net/gh/starfederation/datastar@v1.0.0/bundles/datastar.js\"></script></head><body><main><button data-on:click=\"@get('/get_rando_color')\">Random color</button>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><link rel=\"stylesheet\" href=\"/static/styles.css\"><script type=\"module\" src=\"https://cdn.jsdelivr.net/gh/starfederation/datastar@v1.0.0/bundles/datastar.js\"></script></head><body><main><div data-on:keydown__window=\"evt.key === ' ' && @get('/get_rando_color')\">Random Color</div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
